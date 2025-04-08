fafa127a38661d34057fbaac2b8df5ecb28f7e1b
Timofei Gorlov <72683799+TiM-SyStEm@users.noreply.github.com>
2025-04-08 17:05:43 +0300

    First upload

---
+++ cloning.sh
@@ -0,0 +1,18 @@
+cd dejagnu
+git clone https://git.savannah.gnu.org/git/dejagnu.git
+cd ../newlib
+git clone https://sourceware.org/git/newlib-cygwin.git
+cd ../gdb
+git clone https://sourceware.org/git/binutils-gdb.git
+cd ../qemu
+git clone https://gitlab.com/qemu-project/qemu.git
+cd ../musl
+git clone https://git.musl-libc.org/git/musl
+cd ../spike
+git clone https://github.com/riscv-software-src/riscv-isa-sim.git
+cd ../pk
+git clone https://github.com/riscv-software-src/riscv-pk.git
+cd ../llvm
+git clone https://github.com/llvm/llvm-project.git
+cd ../uclibc-ng
+git clone https://github.com/wbx-github/uclibc-ng.git
